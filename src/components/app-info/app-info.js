639b11545377f368e1ee5a7d7ddeeecae6346ba8
Phil <kshaadoo1991@gmail.com>
2023-08-06 11:17:47 +0300

    Template for app 1.0

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee counter</h1>
+            <h2>Total employees: </h2>
+            <h2>Bonus goes to: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
